0257aef2855c2b921577c29b824b6f6501ad5a32
Unknown6656 <unknown84@gmx.de>
2016-06-13 08:12:00 +0200

    first class / project upload

---
+++ KarnaughMap.cs
@@ -0,0 +1,68 @@
+﻿// COPYRIGHT (C) 2016, UNKNONWN6656
+
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+using System;
+
+namespace BattleOfKarnaugh
+{
+    [DebuggerStepThrough, DebuggerNonUserCode, Serializable, ComVisible(true)]
+    public class KarnaughMap
+    {
+
+        public int[,] map;
+
+        public int Width { internal set; get; }
+        public int Height { internal set; get; }
+        public int VariableCount { internal set; get; }
+
+
+
+        internal void init()
+        {
+            int w = this.Width;
+            int h = this.Height;
+            int v = this.VariableCount;
+
+            map = new int[h, w];
+
+            for (int y = 0; y < h; y++)
+                for (int x = 0; x < w; x++)
+                {
+                    int val = 0;
+
+                    for (int n = 0; n < v; n++)
+                    {
+                        int coffs = 1 << (n / 2);
+                        int coord = n % 2 == 0 ? x : y;
+                        int div = 1 << (n + 1);
+                        bool res = (coord + coffs) % (div * 2) >= div;
+
+                        val |= coord >= coffs ? res ? 1 << n : 0 : 0;
+                    }
+
+                    map[y, x] = val;
+                }
+
+            // TODO : FIX MISTAKES
+        }
+
+        public KarnaughMap(int variables)
+        {
+            if ((this.VariableCount = variables) == 0)
+                this.Width =
+                this.Height = 0;
+            else
+            {
+                this.Width = 1 << (int)Math.Ceiling((double)variables / 2d);
+                this.Height = 1 << (int)Math.Ceiling((variables - 1d) / 2d);
+            }
+
+            this.init();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace BattleOfKarnaugh
+{
+
+    public static class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            KarnaughMap m;
+            
+            for (int n = 1; n < 8; n++)
+            {
+                m = new KarnaughMap(n);
+
+                for (int i = 0, h = m.Height; i < h; i++)
+                    for (int j = 0, w = m.Width; j < w; j++)
+                        Console.Write(m.map[i, j].ToString("x2") + (j >= w - 1 ? " \n" : " "));
+
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
